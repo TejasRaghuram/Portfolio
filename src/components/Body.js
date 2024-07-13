b4bc10494f6940a799cccf2b0b22ad32fcfae80e
TejasRaghuram <tejas.raghuram101@gmail.com>
2024-07-12 19:51:10 -0700

    Created About Section

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ function Body()
     return (
         <div id="body">
             <h2>About</h2>
+            <p>I’m an undergraduate student at Northeastern University with a deep fascination for computer science, especially software development. My interests span across various subfields, including competitive programming, quantum computing, and website design. As a full stack developer, I’ve gained experience in a wide range of languages and frameworks, allowing me to build and contribute to diverse projects. Outside of the world of programming, I enjoy cubing, playing cricket and football, and immersing myself in video games.</p>
             <h2>Projects</h2>
             <h2>Skills</h2>
         </div>
