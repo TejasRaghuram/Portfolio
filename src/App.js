09f9c46ea9d6d9979e6dded519434ce91f5d3429
TejasRaghuram <tejas.raghuram101@gmail.com>
2025-12-15 20:22:41 -0500

    Created Hero Section Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id='hero'>
+        <div id='hero-text'>
+          <p id='hero-greeting' class='hero-sub'>👋 Hi, I'm</p>
+          <h1 id='hero-title'>Tejas Raghuram</h1>
+          <p id='hero-info' class='hero-sub'>CS + Math @ UIUC</p>
+        </div>
+      </div>
     </div>
   );
 }
