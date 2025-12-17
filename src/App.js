93197d08a5db0c15f5896c6379e8eb91ef2e7123
TejasRaghuram <tejas.raghuram101@gmail.com>
2025-12-16 19:37:42 -0500

    Added Hero Text Animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const greetings = ["Hi, I'm", "Bonjour, je m’appelle", "Привет, меня зовут", "Hallo, ich heiße", "ನಮಸ್ಕಾರ, ನನ್ನ ಹೆಸರು"];
+  const info = ["CS + Math @ UIUC", "Software Engineer", "Full Stack Web Developer", "Detail-Oriented Designer", "Quantum Enthusiast"];
+  const [heroIndex, setHeroIndex] = useState(0);
+  const [heroFade, setHeroFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeroFade(false);
+      setTimeout(() => {
+        setHeroIndex((prev) => (prev + 1) % greetings.length);
+        setHeroFade(true);
+      }, 200); 
+    }, 2500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       <div id='hero'>
         <div id='hero-text'>
-          <p id='hero-greeting' class='hero-sub'>👋 Hi, I'm</p>
+          <p id='hero-greeting' class='hero-sub'>👋 <span class={'hero-sub ' + (heroFade ? 'hero-fade-in' : 'hero-fade-out')}>{greetings[heroIndex]}</span></p>
           <h1 id='hero-title'>Tejas Raghuram</h1>
-          <p id='hero-info' class='hero-sub'>CS + Math @ UIUC</p>
+          <p id='hero-info' class={'hero-sub ' + (heroFade ? 'hero-fade-in' : 'hero-fade-out')}>{info[heroIndex]}</p>
         </div>
       </div>
     </div>
